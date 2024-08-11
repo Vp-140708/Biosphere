069b811e1682a59b6cbdf7bf24aecf0fbbb7bed1
Alerta <Zhavoronkovvalex@gmail.com>
2024-08-11 03:33:59 +0300

    12345

---
+++ JS/script2.js
@@ -0,0 +1,4 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Login form submitted! (Database connection will be added later)');
+});
