71b83c6c6955372a005084860e1b8970e186a451
S1get <vlasov_pavel08@mail.ru>
2024-08-18 15:47:02 +0300

    change staff1

--- JS/script1.js
+++ JS/script1.js
@@ -20,6 +20,7 @@ const dropdown = document.querySelector('.dropdown');
 
 hamburgerMenu.addEventListener('click', () => {
     dropdown.classList.toggle('active');
+    
 });
 
 document.getElementById('searchInput').addEventListener('input', function() {
