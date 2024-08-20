3cb57d2eadfbebcb38ba39830347acd2e0ed8724
S1get <vlasov_pavel08@mail.ru>
2024-08-20 10:09:40 +0300

    little changes

--- script10.js
+++ script10.js
@@ -50,3 +50,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const dropdown = document.querySelector('.dropdown');
+
+hamburgerMenu.addEventListener('click', () => {
+    dropdown.classList.toggle('active');
+});
+
+document.getElementById('searchInput').addEventListener('input', function() {
+    const searchValue = this.value.toLowerCase();
+    const specialists = document.querySelectorAll('.specialist-block');
+
+    specialists.forEach(function(specialist) {
+        const name = specialist.querySelector('.specialist-name').textContent.toLowerCase();
+        const description = specialist.querySelector('.specialist-description').textContent.toLowerCase();
+
+        if (name.includes(searchValue) || description.includes(searchValue)) {
+            specialist.style.display = 'block';
+        } else {
+            specialist.style.display = 'none';
+        }
+    });
+});
