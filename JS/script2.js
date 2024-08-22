89476f9790aaa14a6dadb779f683e97c17d06bd3
S1get <vlasov_pavel08@mail.ru>
2024-08-22 13:16:49 +0300

    big changes reviews

--- JS/script2.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Login form submitted! (Database connection will be added later)');
-});
---
+++ JS/script_reviews.js
@@ -0,0 +1,8 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Login form submitted! (Database connection will be added later)');
+});
+document.querySelector('.register-button').addEventListener('click', () => {
+    document.querySelector('.register-section').classList.add('transition-out');
+    document.querySelector('.login-section').classList.add('transition-in');
+  });
