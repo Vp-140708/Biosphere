e2dac2c8fcdf9d58bdb8c49c8d1e4abd3784d6e2
S1get <vlasov_pavel08@mail.ru>
2024-08-22 14:20:50 +0300

    Carousel Elements

--- JS/script_reviews.js
+++ JS/script_reviews.js
@@ -1,9 +1,9 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Login form submitted! (Database connection will be added later)');
-});
-document.querySelector('.register-button').addEventListener('click', () => {
-    document.querySelector('.register-section').classList.add('transition-out');
-    document.querySelector('.login-section').classList.add('transition-in');
-  });
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Login form submitted! (Database connection will be added later)');
+    });
+    document.querySelector('.register-button').addEventListener('click', () => {
+        document.querySelector('.register-section').classList.add('transition-out');
+        document.querySelector('.login-section').classList.add('transition-in');
+    });
 
