c8c59f112a41e71a9fd2bc48bd4aeaede17ca1aa
Alerta <Zhavoronkovvalex@gmail.com>
2024-09-26 20:40:05 +0300

    1

--- JS/script_reviews.js
+++ JS/script_reviews.js
@@ -7,3 +7,31 @@
         document.querySelector('.login-section').classList.add('transition-in');
     });
 
+    const loadMoreBtn = document.querySelector('.load-more-btn');
+    const reviews = document.querySelectorAll('.review-card');
+    let reviewsToShow = 6;
+    
+    // Скрываем отзывы, если их больше 6
+    function hideExtraReviews() {
+        if (reviews.length > reviewsToShow) {
+            loadMoreBtn.style.display = 'block';
+            reviews.forEach((review, index) => {
+                if (index >= reviewsToShow) {
+                    review.style.display = 'none';
+                }
+            });
+        } else {
+            loadMoreBtn.style.display = 'none'; // скрыть кнопку, если отзывов 6 или меньше
+        }
+    }
+    
+    // Показать больше отзывов при нажатии на кнопку
+    loadMoreBtn.addEventListener('click', () => {
+        reviews.forEach(review => {
+            review.style.display = 'block';
+        });
+        loadMoreBtn.style.display = 'none';
+    });
+    
+    // Вызов функции для первоначального скрытия лишних отзывов
+    hideExtraReviews();
